e8ca415e2c3000d9b66c7085b44ced04853ba954
seaboyz <seaboyz@hotmail.com>
2020-10-13 22:28:43 -0500

    Add productDetailsReducer to prodecReducer.js

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,18 +1,36 @@
 import {
-    PRODUCT_LIST_SUCCESS,
-    PPRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_FIAL,
+  PRODUCT_LIST_SUCCESS,
+  PPRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_FIAL,
+  PPRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FIAL,
 } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
-    switch (action.type) {
-        case PPRODUCT_LIST_REQUEST:
-            return { loading: true, products: [] }
-        case PRODUCT_LIST_SUCCESS:
-            return { loading: false, products: action.payload }
-        case PRODUCT_LIST_FIAL:
-            return { loading: false, error: action.payload }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case PPRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] }
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload }
+    case PRODUCT_LIST_FIAL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PPRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state }
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload }
+    case PRODUCT_DETAILS_FIAL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
 }
