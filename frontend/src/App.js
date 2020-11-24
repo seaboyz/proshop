97451277626e607ac5a95ede302f2e72ddefe092
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-24 15:49:09 -0600

    Add Header.js and Footer.js to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
     return (
         <>
-            <h1>Welcome To ProShop</h1>
+            <Header />
+            <main>
+                <h1>Welcome To ProShop</h1>
+            </main>
+            <Footer />
         </>
     )
 }
