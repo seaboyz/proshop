87bafaa8b5b06b6b47b47d67e0895baf4531b1b7
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-24 16:57:05 -0600

    Finish Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 function Header() {
-    return <header>Header</header>
+    return (
+        <header>
+            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                    <Navbar.Collapse id='basic-navbar-nav'>
+                        <Nav className='ml-auto'>
+                            {/* Move two links to the right */}
+                            <Nav.Link href='/cart'>Cart</Nav.Link>
+                            <Nav.Link href='/login'>Sign In</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
 }
 
 export default Header
