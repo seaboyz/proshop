126ff3d957b6e127e7a8fba62097172edc2a88a5
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-24 16:53:53 -0600

    Finish Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
-    return <footer>Footer</footer>
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                        <Col>Copyright &copy; Proshop</Col>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
 
 export default Footer
