bd444a011df914494794e098c422009aa4a93446
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-24 17:07:08 -0600

    import Container to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-const App = () => {
+
+function App() {
     return (
         <>
             <Header />
             <main>
-                <h1>Welcome To ProShop</h1>
+                <Container className='text-center py-3'>
+                    <h1>Welcome To ProShop</h1>
+                </Container>
             </main>
             <Footer />
         </>
