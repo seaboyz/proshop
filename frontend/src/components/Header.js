1218ad57e4a5fcb863db98922aed8f72cba9eadc
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-24 17:03:49 -0600

    Add font awesome to Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,8 +11,12 @@ function Header() {
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ml-auto'>
                             {/* Move two links to the right */}
-                            <Nav.Link href='/cart'>Cart</Nav.Link>
-                            <Nav.Link href='/login'>Sign In</Nav.Link>
+                            <Nav.Link href='/cart'>
+                                <i className='fas fa-shopping-cart'></i> Cart
+                            </Nav.Link>
+                            <Nav.Link href='/login'>
+                                <i className='fas fa-user'></i> Sign In
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
