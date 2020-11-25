5bb9a0bdade7152bd9a3baedbe702b7a1ccd85e6
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-25 15:50:50 -0600

    Add HomeScreen.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -9,7 +10,7 @@ function App() {
             <Header />
             <main>
                 <Container className='text-center py-3'>
-                    <h1>Welcome To ProShop</h1>
+                    <HomeScreen />
                 </Container>
             </main>
             <Footer />
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import products from '../products'
+import Product from '../components/Product'
 
 function HomeScreen() {
     return (
@@ -9,7 +10,7 @@ function HomeScreen() {
             <Row>
                 {products.map(product => (
                     <Col sm={12} md={6} lg={4} xl={3}>
-                        <Product />
+                        <Product product={product} />
                     </Col>
                 ))}
             </Row>
