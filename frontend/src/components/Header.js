0ce60693feac9b561280cc78f66a52cc6fbb58c7
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-26 21:01:03 -0600

    Replace a tag and href with Link and to

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <header>
             <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href='#home'>PROSHOP</Navbar.Brand>
                     <Navbar.Toggle aria-controls='basic-navbar-nav' />
                     <Navbar.Collapse id='basic-navbar-nav'>
                         <Nav className='ml-auto'>
