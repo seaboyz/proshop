e280831d3b33fc8b855181ae3050e25707f372be
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-27 16:36:53 -0600

    Add ProductScreen.js

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-function ProductScreen() {
-    return <div>Product</div>
+function ProductScreen({ match }) {
+    const product = products.find(p => p._id === match.params.id)
+    return (
+        <>
+            <Link className='btn btn-light my-3' to='/'>
+                Go Back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image
+                        src={product.image}
+                        alt={product.name}
+                        title={product.name}
+                        fluid
+                    ></Image>
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>{product.name}</h2>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating
+                                rating={product.rating}
+                                numReviews={`${product.numReviews} reviews`}
+                            />
+                        </ListGroup.Item>
+                        <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+                        <ListGroup.Item>
+                            Description: {product.description}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+            </Row>
+        </>
+    )
 }
 
 export default ProductScreen
