0f28c6b92b19500d27007bd9e52bdc1b468dc2e5
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-11-28 19:46:19 -0600

    Add docenv module

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 
+const dotenv = require('dotenv')
+
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -17,4 +21,11 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log('Server is running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(
+        `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+)
