5501b80e9da60129b8e0d39c7ab0f91877bcaeb1
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-12-06 21:58:28 -0600

    Dispatch from CartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { addToCart } from '../actions/cartActions'
 
-function CartScreen() {
+function CartScreen({ match, location, history }) {
+    const productId = match.params.id
+    // get qty either from the url or default 1
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+
+    const { cartItems } = cart
+
+    useEffect(() => {
+        productId && dispatch(addToCart(productId, qty))
+    }, [dispatch, productId, qty])
+
     return <div>Cart</div>
 }
 
