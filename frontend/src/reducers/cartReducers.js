ad2002e8fd0a90c9ef8ea68bf40dc87fa4efc7a0
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-12-06 20:17:03 -0600

    Add cartReducers

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,25 @@
+import { CART_ADD_TIEM } from '../constants/cartConstants'
+
+export function cartReducer(state = { cartItems: [] }, action) {
+    switch (action.type) {
+        case CART_ADD_TIEM:
+            const item = action.payload
+            const existItem = state.cartItems.find(
+                x => x.product.id === item.product
+            )
+            if (existItem) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.map(x =>
+                        x.product === existItem.product ? item : x
+                    ),
+                }
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item],
+                }
+            }
+    }
+    return state
+}
