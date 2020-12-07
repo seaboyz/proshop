4afe138671ed264be3b894489b37ad1da704fa0b
Qiang Gao <qinaggao@Qiangs-MacBook-Air.local>
2020-12-06 20:43:35 -0600

    Add cartActions

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+import { CART_ADD_TIEM } from '../constants/cartConstants'
+
+export function addToCart(id, qty) {
+    return async function (dispatch, getState) {
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type: CART_ADD_TIEM,
+            payload: {
+                product: data._id,
+                name: data.name,
+                image: data.image,
+                price: data.price,
+                countInStock: data.countInStock,
+                qty,
+            },
+        })
+        localStorage.setItem(
+            'carItems',
+            JSON.stringify(getState().cart.cartItems)
+        )
+    }
+}
