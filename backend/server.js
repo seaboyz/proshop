86702964056c692568d25464702c3d298f07a51a
seaboyz <seaboyz@hotmail.com>
2021-07-30 16:24:27 -0500

    Connect server to database

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,32 @@
-import express from "express";
-import dotenv from "dotenv";
-import products from "./data/products.js";
+import express from "express"
 
-dotenv.config();
+import dotenv from "dotenv"
 
-const app = express();
+import products from "./data/products.js"
+import connectDB from "./config/db.js"
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
 
 app.get("/", (req, res) => {
-  res.send("API is running");
-});
+  res.send("API is running")
+})
 
 app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+  res.json(products)
+})
 
 app.get("/api/products/:id", (req, res) => {
-  const product = products.find(p => p._id === req.params.id);
-  res.json(product);
-});
+  const product = products.find(p => p._id === req.params.id)
+  res.json(product)
+})
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
   console.log(`Server running on ${process.env.NODE_ENV} mode port ${PORT}`)
-);
+)
