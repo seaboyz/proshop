1cdcac485ecf889e68b8b03a6fd8ac84cb0b416c
seaboyz <seaboyz@hotmail.com>
2021-07-30 16:33:35 -0500

    Add colors to command line

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express"
-
 import dotenv from "dotenv"
+import colors from "colors"
 
 import products from "./data/products.js"
 import connectDB from "./config/db.js"
@@ -28,5 +28,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running on ${process.env.NODE_ENV} mode port ${PORT}`)
+  console.log(
+    `Server running on ${process.env.NODE_ENV} mode port ${PORT}`.yellow.bold
+  )
 )
