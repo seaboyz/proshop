ae82f334508bbcc7758a792bac9fbf829c5675a2
seaboyz <seaboyz@hotmail.com>
2021-07-30 20:20:13 -0500

    Add express error middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from "colors"
 
 import connectDB from "./config/db.js"
 import productsRoutes from "./routes/productRoutes.js"
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
 
 dotenv.config()
 
@@ -15,7 +16,15 @@ app.get("/", (req, res) => {
   res.send("API is running")
 })
 
+// requests to /api/products will be sent to productsRouter
 app.use("/api/products", productsRoutes)
+
+// https://expressjs.com/en/guide/error-handling.html
+// express error handling
+app.use(notFound)
+
+// error handling middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
