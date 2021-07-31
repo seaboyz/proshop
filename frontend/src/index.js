95fb16fce0414ac26539f7804b97d6ec127803f1
seaboyz <seaboyz@hotmail.com>
2021-07-31 10:10:13 -0500

    redux boiler code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./bootstrap.min.css";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import "./bootstrap.min.css"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import store from "./store"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
-);
+)
 
-reportWebVitals();
+reportWebVitals()
