189e3217a4aec39d78c55e8e40b15de70dd14269
seaboyz <seaboyz@hotmail.com>
2021-08-01 19:22:16 -0500

    Add removeFromCart action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
+// getState:https://github.com/reduxjs/redux-thunk#motivation
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`)
 
@@ -20,5 +21,14 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
   // update localStorage with the current state tree of your application
   // https://redux.js.org/api/store#getstate
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = id => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  })
+
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
