289353bf54fa8ec361e9216d7aff0006fd7e34d9
seaboyz <seaboyz@hotmail.com>
2021-08-04 08:58:14 -0500

    Add registerUser in userControler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,3 +44,36 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found')
   }
 })
+
+// @desc  Register a new user
+// @route Post /api/users/
+// @access Public
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
