ca7f75e8bd6fb504de33ae2b6e90303381ec329b
seaboyz <seaboyz@hotmail.com>
2021-08-06 14:24:45 -0500

    Add savePaymentMethod action

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
-  CART_SAVE_SHIPPING_ADDRESS_REQUEST,
+  CART_SAVE_PAYMENT_METHOD,
+  CART_SAVE_SHIPPING_ADDRESS,
 } from '../constants/cartConstants'
 
 // getState:https://github.com/reduxjs/redux-thunk#motivation
@@ -39,8 +40,16 @@ export const removeFromCart = (id) => (dispatch, getState) => {
 
 export const saveShippingAdrees = (data) => (dispatch) => {
   dispatch({
-    type: CART_SAVE_SHIPPING_ADDRESS_REQUEST,
+    type: CART_SAVE_SHIPPING_ADDRESS,
     payload: data,
   })
   localStorage.setItem('shippingAddress', JSON.stringify(data))
+}
+
+export const savePaymentMethod = (data) => (dispatch) => {
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD,
+    payload: data,
+  })
+  localStorage.setItem('paymentMethod', JSON.stringify(data))
 }
