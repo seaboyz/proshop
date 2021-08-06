5b54f5de48426a59449c1387707b646964a657ba
seaboyz <seaboyz@hotmail.com>
2021-08-06 14:23:35 -0500

    Add CART_SAVE_PAYMENT_METHOD case to cartReducer

--- frontend/src/redux/reducers/cartReducers.js
+++ frontend/src/redux/reducers/cartReducers.js
@@ -1,7 +1,8 @@
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
-  CART_SAVE_SHIPPING_ADDRESS_REQUEST,
+  CART_SAVE_PAYMENT_METHOD,
+  CART_SAVE_SHIPPING_ADDRESS,
 } from '../constants/cartConstants'
 
 export const cartReducer = (
@@ -38,10 +39,16 @@ export const cartReducer = (
         cartItems: state.cartItems.filter((item) => item.id !== id),
       }
 
-    case CART_SAVE_SHIPPING_ADDRESS_REQUEST:
+    case CART_SAVE_SHIPPING_ADDRESS:
       return {
         ...state,
         shippingAddress: action.payload,
+      }
+
+    case CART_SAVE_PAYMENT_METHOD:
+      return {
+        ...state,
+        paymentMethod: action.payload,
       }
 
     default:
