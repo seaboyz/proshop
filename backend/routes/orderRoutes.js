792c45613a32a2a8e9b0cf541d18cd51ffad82ca
seaboyz <seaboyz@hotmail.com>
2021-08-06 21:03:33 -0500

    Add orderController

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { addOrderItems } from '../controllers/orderController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+// @disc Create new order
+// @route POST api/orders
+// @access
+
+router.route('/').post(protect, addOrderItems)
+
+export default router
