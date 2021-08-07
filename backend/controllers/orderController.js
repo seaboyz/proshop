7b1a0a0d688c21ecf6584f6698ac836b365778ab
seaboyz <seaboyz@hotmail.com>
2021-08-07 16:11:28 -0500

    Add getOrderById controler to orderController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,10 @@
+import expressAsyncHandler from 'express-async-handler'
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
 // @disc Create new order
 // @route POST api/orders
-// @access
+// @access private
 
 export const addOrderItems = asyncHandler(async (req, res) => {
   const {
@@ -34,5 +35,25 @@ export const addOrderItems = asyncHandler(async (req, res) => {
 
     const createdOrder = await order.save()
     res.status(201).json(createdOrder)
+  }
+})
+
+// @disc GET order by id
+// @route GET api/orders/:id
+// @access private
+export const getOrderById = expressAsyncHandler(async (req, res) => {
+  const id = req.params.id
+
+  const order = await Order.findById(id).populate(
+    'user',
+    'name email'
+  ) /* populate another document 'user' to get 'name' and 'email' fields */
+  // https://mongoosejs.com/docs/populate.html#populate_multiple_documents
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
   }
 })
