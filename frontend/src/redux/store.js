cc0c40bef500ddf9cb1cbf663376549d7064f757
seaboyz <seaboyz@hotmail.com>
2021-08-07 21:23:55 -0500

    Add orderDetailsReducer to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -12,7 +12,10 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
 } from './reducers/userReducers'
-import { orderCreatReducer } from './reducers/orderReducers'
+import {
+  orderCreatReducer,
+  orderDetailsReducer,
+} from './reducers/orderReducers'
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -23,6 +26,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreatReducer,
+  orderDetails: orderDetailsReducer,
 })
 
 // get data from locastorage
